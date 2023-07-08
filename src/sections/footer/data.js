9ae57f8cb3894f3579a9729ade93f68581c7a284
Imayush798 <ayushsahu798@gmail.com>
2023-07-08 21:05:37 +0530

    update

---
+++ src/sections/footer/data.js
@@ -0,0 +1,11 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiFillLinkedin} from 'react-icons/ai'
+
+const data = [
+    {id: 1, link: 'https://www.linkedin.com/in/ayush-kumar-956286203', icon: <AiFillLinkedin/>},
+    {id: 2, link: 'https://twitter.com/imAyush798?t=SlfPqmR_Jusc1YQnxVEBEA&s=08', icon: <AiOutlineTwitter/>},
+    {id: 3, link: 'https://instagram.com/ayush.sahu.26?igshid=MzNlNGNkZWQ4Mg==', icon: <AiOutlineInstagram/>}
+
+]
+export default data
